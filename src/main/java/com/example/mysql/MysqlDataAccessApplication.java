21b81e8714463dc8aff136a900915c1ed51258c5
unknown <s.raghavender@gmail.com>
2023-03-05 21:52:28 +0530

    spring database example
     spring database example

---
+++ src/main/java/com/example/mysql/MysqlDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlDataAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlDataAccessApplication.class, args);
+	}
+
+}
